0c259f768cddbe4ae7f5d9c568b92ac722de7917
Jacob Welander Jensen <64834767+Welander1994@users.noreply.github.com>
2022-02-28 12:26:36 +0100

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,10 @@
+console.log("Is the script running, then get after it...")
+
+function ClickBygning1() {
+    var x = document.getElementById("bygningtekst1");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
